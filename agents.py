ad6f968bec5fc0fab923fee91aee0f168d489161
Hassan Ahmed <hassanahmed40005@gmail.com>
2026-06-11 14:33:01 +0500

    Update agents and requirements

--- agents.py
+++ agents.py
@@ -1,5 +1,5 @@
 from langchain.agents import create_agent
-from langchain_openai import OpenAI
+
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
